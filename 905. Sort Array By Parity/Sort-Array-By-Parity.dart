fb006b6cbe47ba6901869fd92b2a0d600e063505
Abdallah Mohamed <115114629+iABDALLAHM@users.noreply.github.com>
2026-05-24 12:41:43 +0300

    [905] [Time Beats: 100.00%] [Memory Beats: 43.75%] - LeetPush

--- 905. Sort Array By Parity/Sort-Array-By-Parity.dart
+++ 905. Sort Array By Parity/Sort-Array-By-Parity.dart
@@ -1,17 +1,19 @@
 1class Solution {
 2  List<int> sortArrayByParity(List<int> nums) {
-3      int pointer1 = 0;
-4        int pointer2 = 0;
-5
-6    while (pointer2 < nums.length) {
-7    if (nums[pointer2] % 2 == 0) {
-8      int temp = nums[pointer1];
-9      nums[pointer1] = nums[pointer2];
-10      nums[pointer2] = temp;
-11      pointer1++;
-12    }
-13    pointer2++;
-14  }
-15  return nums;
-16  }
-17}
+3
+4    int pointer1 = 0;
+5    int pointer2 = 0;
+6
+7    while (pointer2 < nums.length) {
+8    if (nums[pointer2] % 2 == 0) {
+9      int temp = nums[pointer1];
+10      nums[pointer1] = nums[pointer2];
+11      nums[pointer2] = temp;
+12      pointer1++;
+13    }
+14    pointer2++;
+15    }
+16    return nums;
+17  }
+18}
+19
