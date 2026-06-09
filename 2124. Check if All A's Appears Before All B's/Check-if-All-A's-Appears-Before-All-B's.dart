e8c021eb2959be7be670540e24fac65cd6221761
Abdallah Mohamed <115114629+iABDALLAHM@users.noreply.github.com>
2026-06-09 09:20:04 +0300

    [2124] [Time Beats: 100.00%] [Memory Beats: 16.67%] - LeetPush

---
+++ 2124. Check if All A's Appears Before All B's/Check-if-All-A's-Appears-Before-All-B's.dart
@@ -0,0 +1,15 @@
+1class Solution {
+2  bool checkString(String s) {
+3      bool seenB = false;
+4  for (int i = 0; i < s.length; i++) {
+5    if (s[i] == "a") {
+6      if (seenB == true) {
+7        return false;
+8      }
+9    } else {
+10      seenB = true;
+11    }
+12  }
+13  return true;
+14  }
+15}
