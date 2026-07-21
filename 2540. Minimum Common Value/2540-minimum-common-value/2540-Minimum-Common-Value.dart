6e8053546eab7524d6891f785ceee2bf5533a1f8
Abdallah Mohamed <115114629+iABDALLAHM@users.noreply.github.com>
2026-07-21 11:05:56 +0300

    [2540] [Time Beats: 23.08%] [Memory Beats: 7.69%] - LeetPush

--- 2540. Minimum Common Value/2540-minimum-common-value/2540-Minimum-Common-Value.dart
+++ 2540. Minimum Common Value/2540-minimum-common-value/2540-Minimum-Common-Value.dart
@@ -1,34 +1,20 @@
 class Solution {
   int getCommon(List<int> nums1, List<int> nums2) {
-      Set setNums1 = {};
-  Set setNums2 = {};
 
-  for (int i = 0; i < nums1.length; i++) {
-    setNums1.add(nums1[i]);
-  }
-
-  for (int i = 0; i < nums2.length; i++) {
-    setNums2.add(nums2[i]);
-  }
-
-  print(setNums2);
-  print(setNums1);
+    Set setNums1 = nums1.toSet();
 
   int minValue = 999999999999999999;
 
-  for (int i = 0; i < nums1.length; i++) {
-    if (setNums1.contains(nums1[i]) && setNums2.contains(nums1[i])) {
-      if (nums1[i] < minValue) {
-        minValue = nums1[i];
+  for (int i = 0; i < nums2.length; i++) {
+    if (setNums1.contains(nums2[i])) {
+      if (nums2[i] < minValue) {
+        minValue = nums2[i];
       }
     }
   }
 
- if(minValue==999999999999999999){
-    return -1;
- }else {
-    return minValue;
- }
+
+  return minValue == 999999999999999999 ? -1 : minValue;
 
   }
 }
