b9c30f65b16cb14636dd8e01d185ddb994b8dc04
Abdallah Mohamed <115114629+iABDALLAHM@users.noreply.github.com>
2026-07-24 22:40:02 +0300

    [2441] [Time Beats: -%] [Memory Beats: 100.00%] - LeetPush

--- 2441. Largest Positive Integer That Exists With Its Negative/2441-largest-positive-integer-that-exists-with-its-negative/2441-Largest-Positive-Integer-That-Exists-With-Its-Negative.dart
+++ 2441. Largest Positive Integer That Exists With Its Negative/2441-largest-positive-integer-that-exists-with-its-negative/2441-Largest-Positive-Integer-That-Exists-With-Its-Negative.dart
@@ -1,29 +1,27 @@
-class Solution {
-  int findMaxK(List<int> nums) {
-  Set<int> setOfPositive = {};
-  Set<int> setOfNegative = {};
+    class Solution {
+    int findMaxK(List<int> nums) {
+    Set<int> setOfPositive = {};
+    Set<int> setOfNegative = {};
 
-  int result = -1;
+    int result = -1;
 
-  for (int i = 0; i < nums.length; i++) {
-    if (nums[i] > 0) {
-      setOfPositive.add(nums[i]);
-    } else {
-      setOfNegative.add(nums[i]);
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+        setOfPositive.add(nums[i]);
+        } else {
+        setOfNegative.add(nums[i]);
+        }
     }
-  }
-  print(setOfPositive);
-  print(setOfNegative);
+    print(setOfPositive);
+    print(setOfNegative);
 
-  for (var element in setOfPositive) {
-    if (setOfPositive.contains(element) &&
-        setOfNegative.contains(-element) &&
-        element > result) {
-      result = element;
+    for (var element in setOfPositive) {
+        if (setOfNegative.contains(-element) && element > result) {
+        result = element;
+        }
     }
-  }
 
-  return result;
+    return result;
 
-  }
+    }
 }
