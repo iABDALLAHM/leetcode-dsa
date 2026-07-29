96384ae08eec462e93528eac140736cbf8f49997
Abdallah Mohamed <115114629+iABDALLAHM@users.noreply.github.com>
2026-07-29 10:40:29 +0300

    [496] [Time Beats: 20.00%] [Memory Beats: 90.00%] - LeetPush

---
+++ 496. Next Greater Element I/496-next-greater-element-i/496-Next-Greater-Element-I.dart
@@ -0,0 +1,23 @@
+class Solution {
+  List<int> nextGreaterElement(List<int> nums1, List<int> nums2) {
+  List<int> result = [];
+  for (int i = 0; i < nums1.length; i++) {
+    bool found = false;
+    for (int j = 0; j < nums2.length; j++) {
+      if (nums1[i] == nums2[j]) {
+        for (int k = j + 1; k < nums2.length; k++) {
+          if (nums2[k] > nums1[i]) {
+            result.add(nums2[k]);
+            found = true;
+            break;
+          }
+        }
+      }
+    }
+    if (!found) {
+      result.add(-1);
+    }
+  }
+  return result;
+  }
+}
