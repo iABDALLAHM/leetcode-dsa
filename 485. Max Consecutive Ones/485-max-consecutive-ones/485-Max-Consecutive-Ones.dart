ff15df667c8e5f358d50f8d403caf61e8075dda6
Abdallah Mohamed <115114629+iABDALLAHM@users.noreply.github.com>
2026-08-01 11:28:39 +0300

    [485] [Time Beats: 100.00%] [Memory Beats: 43.14%] - LeetPush

---
+++ 485. Max Consecutive Ones/485-max-consecutive-ones/485-Max-Consecutive-Ones.dart
@@ -0,0 +1,21 @@
+class Solution {
+  int findMaxConsecutiveOnes(List<int> nums) {
+
+  int counter = 0;
+  int maxCounter = 0;
+
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 1) {
+      counter++;
+      if (counter > maxCounter) {
+        maxCounter = counter;
+      }
+    } else {
+      counter = 0;
+    }
+  }
+
+  return maxCounter;
+
+  }
+}
