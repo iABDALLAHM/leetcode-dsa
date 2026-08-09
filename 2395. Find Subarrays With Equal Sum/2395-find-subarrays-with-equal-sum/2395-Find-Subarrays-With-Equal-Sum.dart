348b2c2ae42e35c9e1eee8d24b808148bf2114b1
Abdallah Mohamed <115114629+iABDALLAHM@users.noreply.github.com>
2026-08-09 11:49:35 +0300

    [2395] [Time Beats: 100.00%] [Memory Beats: -%] - LeetPush

--- 2395. Find Subarrays With Equal Sum/2395-find-subarrays-with-equal-sum/2395-Find-Subarrays-With-Equal-Sum.dart
+++ 2395. Find Subarrays With Equal Sum/2395-find-subarrays-with-equal-sum/2395-Find-Subarrays-With-Equal-Sum.dart
@@ -1,25 +1,22 @@
 class Solution {
   bool findSubarrays(List<int> nums) {
-      int counterOfSubArray = 0;
-  Set<int> setOfSum = {};
+
+    int counterOfSubArray = 0;
+    Set<int> setOfSum = {};
+
   for (int i = 0; i < nums.length - 1; i++) {
-    List<int> subList = nums.sublist(
-      i,
-      (i + 2) > nums.length ? nums.length : (i + 2),
-    );
 
-    int sumOfCurrentSubArray = 0;
-
-    for (int j = 0; j < subList.length; j++) {
-      sumOfCurrentSubArray += subList[j];
-    }
+    int sumOfCurrentSubArray = nums[i] + nums[i + 1];
 
     if (setOfSum.contains(sumOfCurrentSubArray)) {
       counterOfSubArray++;
     } else {
       setOfSum.add(sumOfCurrentSubArray);
     }
+
   }
+
+
  return counterOfSubArray > 0 ? true : false;
   }
 }
