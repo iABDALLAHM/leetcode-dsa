0e52be557ef2e5797bb8c3a9aa27e7282b21aad3
Abdallah Mohamed <115114629+iABDALLAHM@users.noreply.github.com>
2026-08-10 20:39:49 +0300

    [2710] [Time Beats: 100.00%] [Memory Beats: 100.00%] - LeetPush

---
+++ 2710. Remove Trailing Zeros From a String/2710-Remove-Trailing-Zeros-From-a-String.dart
@@ -0,0 +1,16 @@
+class Solution {
+  String removeTrailingZeros(String num) {
+    String result = "";
+
+  for (int i = num.length - 1; i >= 0; i--) {
+    if (int.parse(num[i]) != 0) {
+      result = num.substring(0, i+1);
+      break;
+    }
+  }
+
+  return result;
+
+  }
+}
+
