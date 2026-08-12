b41aae664285d8649315df09d755bc9085c23ee7
Abdallah Mohamed <115114629+iABDALLAHM@users.noreply.github.com>
2026-08-12 12:16:01 +0300

    [414] [Time Beats: -%] [Memory Beats: 10.00%] - LeetPush

---
+++ 414. Third Maximum Number/414-third-maximum-number/414-Third-Maximum-Number.dart
@@ -0,0 +1,26 @@
+class Solution {
+  int thirdMax(List<int> nums) {
+  nums.sort();
+  List<int> newNums = [];
+  Set<int> setOfNums = nums.toSet();
+
+  for (int i = 0; i < nums.length; i++) {
+    if (!newNums.contains(nums[i])) {
+      newNums.add(nums[i]);
+    }
+  }
+
+  int maxResult = 0;
+  if (newNums.length >= 3) {
+    maxResult = newNums[newNums.length - 3];
+  } else if (newNums.length <= 2) {
+    for (var num in setOfNums) {
+      if (num > maxResult) {
+        maxResult = num;
+      }
+    }
+  }
+
+  return maxResult;
+  }
+}
