93aa289d084df9025a2b1047ccb29203e61faae4
Abdallah Mohamed <115114629+iABDALLAHM@users.noreply.github.com>
2026-08-17 10:32:35 +0300

    [3019] [Time Beats: 100.00%] [Memory Beats: 100.00%] - LeetPush

---
+++ 3019. Number of Changing Keys/3019-number-of-changing-keys/3019-Number-of-Changing-Keys.dart
@@ -0,0 +1,12 @@
+class Solution {
+  int countKeyChanges(String s) {
+      var stringS = s.toLowerCase();
+  int counter = 0;
+  for (int i = 0; i < stringS.length; i++) {
+    if (i > 0 && stringS[i] != stringS[i - 1]) {
+      counter++;
+    }
+  }
+  return counter;
+  }
+}
