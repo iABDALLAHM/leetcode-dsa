8084385ea8faa6b523b28706aaa35ff629456570
Abdallah Mohamed <115114629+iABDALLAHM@users.noreply.github.com>
2026-09-19 14:39:50 +0300

    [3151] [Time Beats: 100.00%] [Memory Beats: 100.00%] - LeetPush

--- 3151. Special Array I/3151-special-array-i/3151-Special-Array-I.dart
+++ 3151. Special Array I/3151-special-array-i/3151-Special-Array-I.dart
@@ -1,19 +1,22 @@
 class Solution {
   bool isArraySpecial(List<int> nums) {
-     if (nums.length == 1){
-         return true;
+
+    if (nums.length == 1){
+    return true;
     }
 
-      bool result = true;
-  for (int i = 1; i < nums.length; i++) {
+    bool result = true;
+    for (int i = 1; i < nums.length; i++) {
     int prevEle = nums[i - 1];
     int currentElement = nums[i];
     if (currentElement % 2 == prevEle % 2) {
       result = false;
-    }
-  }
+      break;
 
+    }
+}
 
     return result;
+
   }
 }
