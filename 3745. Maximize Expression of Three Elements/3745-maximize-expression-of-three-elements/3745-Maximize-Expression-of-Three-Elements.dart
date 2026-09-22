566fa6410576ecf88098a9ac5b08f11b8ec7394d
Abdallah Mohamed <115114629+iABDALLAHM@users.noreply.github.com>
2026-09-22 11:07:15 +0300

    [3745] [Time Beats: 100.00%] [Memory Beats: 100.00%] - LeetPush

---
+++ 3745. Maximize Expression of Three Elements/3745-maximize-expression-of-three-elements/3745-Maximize-Expression-of-Three-Elements.dart
@@ -0,0 +1,12 @@
+class Solution {
+  int maximizeExpressionOfThree(List<int> nums) {
+      nums.sort();
+
+  int a = nums[nums.length - 1];
+  int b = nums[nums.length - 2];
+
+  int c = nums[0];
+
+  return a + b - c;
+  }
+}
